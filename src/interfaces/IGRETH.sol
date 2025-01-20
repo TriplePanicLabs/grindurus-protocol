b42c593d9d76978d725a8e3a0c487ba3c0fa3a3b
Pozzitron1337 <the.vaho1337@gmail.com>
2025-01-20 13:26:20 +0200

    updated interface

--- src/interfaces/IGRETH.sol
+++ src/interfaces/IGRETH.sol
@@ -28,7 +28,7 @@ interface IGRETH is IToken {
 
     function burn(uint256 amount, address token) external payable returns (uint256 tokenAmount);
 
-    function burn(uint256[] memory amounts, address[] memory tokens) external payable returns (uint256 tokenAmount);
+    function batchBurn(uint256[] memory amounts, address[] memory tokens) external payable returns (uint256 tokenAmount);
 
     function share(uint256 amount, address token) external view returns (uint256);
 
