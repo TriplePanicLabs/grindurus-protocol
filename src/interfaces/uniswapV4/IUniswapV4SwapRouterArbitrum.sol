e7e0d3118a02e97c2d09d8cda3c4f05a9f924397
Pozzitron1337 <the.vaho1337@gmail.com>
2025-04-10 00:56:49 +0300

    added blank interface for uniV4

---
+++ src/interfaces/uniswapV4/IUniswapV4SwapRouterArbitrum.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+/// @title UniswapV4 Router token swapping functionality
+/// @notice Functions for swapping tokens via Uniswap V4
+/// @dev network Arbitrum mainnet (chainId == 42161)
+interface IUniswapV4SwapRouterArbitrum {
+   
+}
