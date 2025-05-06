8dda4b05ddea99587c429b41580a1bdd11ee3d1c
Pozzitron1337 <the.vaho1337@gmail.com>
2025-05-06 14:14:16 +0300

    instantiated addresses in constructor

--- src/registries/RegistryArbitrum.sol
+++ src/registries/RegistryArbitrum.sol
@@ -8,14 +8,20 @@ import {Registry, PriceOracleInverse} from "./Registry.sol";
 /// @dev stores array of strategy ids, strategy pairs, quote tokens, base tokens, and bounded oracles
 contract RegistryArbitrum is Registry {
 
-    address public oracleWethUsdArbitrum = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612; // chainlink WETH/USD oracle;
+    address public oracleWethUsdArbitrum;
 
-    address public wethArbitrum = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
-    address public usdtArbitrum = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
-    address public usdcArbitrum = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address public wethArbitrum;
+    address public usdtArbitrum;
+    address public usdcArbitrum;
 
     /// @param _poolsNFT address of poolsNFT
     constructor(address _poolsNFT) Registry(_poolsNFT) {
+        oracleWethUsdArbitrum = 0x639Fe6ab55C921f74e7fac1ee960C0B6293ba612;
+
+        wethArbitrum = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+        usdtArbitrum = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
+        usdcArbitrum = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+
         oracles[usdtArbitrum][wethArbitrum] = oracleWethUsdArbitrum;
         oracles[usdcArbitrum][wethArbitrum] = oracleWethUsdArbitrum;
         PriceOracleInverse priceOracleInverse = new PriceOracleInverse(oracleWethUsdArbitrum);
--- src/registries/RegistryBase.sol
+++ src/registries/RegistryBase.sol
@@ -8,13 +8,18 @@ import {Registry, PriceOracleInverse} from "./Registry.sol";
 /// @dev stores array of strategy ids, strategy pairs, quote tokens, base tokens, and bounded oracles
 contract RegistryBase is Registry {
 
-    address public oracleWethUsdBase = 0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70; // chainlink WETH/USD oracle;
+    address public oracleWethUsdBase;
 
-    address public wethBase = 0x4200000000000000000000000000000000000006;
-    address public usdcBase = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+    address public wethBase;
+    address public usdcBase;
 
     /// @param _poolsNFT address of poolsNFT
     constructor(address _poolsNFT) Registry(_poolsNFT) {
+        oracleWethUsdBase = 0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70; // chainlink WETH/USD oracle;
+
+        wethBase = 0x4200000000000000000000000000000000000006;
+        usdcBase = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913;
+        
         oracles[usdcBase][wethBase] = oracleWethUsdBase;
         PriceOracleInverse priceOracleInverse = new PriceOracleInverse(oracleWethUsdBase);
         oracles[wethBase][usdcBase] = address(priceOracleInverse);
